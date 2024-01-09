e4ed34b429c8555d0e567739605009bce3dd8758
airRash86 <zakirov-rashid@yandex.ru>
2024-01-09 19:43:13 +0300

    продолжение обучения4

--- !!!DONT FORGET Apply.py
+++ !!!DONT FORGET Apply.py
@@ -1,7 +1,13 @@
 import json
+"""
+A "clumsy " script for tracking where the last commit/PR was made in REMOTE.
+It will work if I don't forget to apply it
+(I'm not applying the PreCOMMIT yet: I haven't figured out this topic yet)
+"""
 
-with open('loc_name.TXT', 'r', encoding='utf-8') as File:
-    loc_name_value = File.readline().strip()  
+with open('loc_name.json', 'r') as file:
+    loc_name_data = json.load(file)  
+    loc_name_value = loc_name_data['locName']  
 
 with open('локация.json', 'r', encoding='utf-8') as File:
     data = json.load(File)
